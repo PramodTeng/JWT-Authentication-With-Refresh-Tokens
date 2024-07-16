242d43e269e4babb6c934f3c6cf443fef26db752
Pramod Dhimal <pramoddhimal@vurilo.com>
2024-07-16 16:21:06 +0545

    Add project files.

---
+++ RefreshTokenJwt/Auth/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RefreshTokenJwt.Auth
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+
+}
---
+++ RefreshTokenJwt/Model/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace RefreshTokenJwt.Model
+{
+    public class Response
+    {
+        public string? Status { get; set; }
+        public string? Message { get; set; }
+    }
+}
